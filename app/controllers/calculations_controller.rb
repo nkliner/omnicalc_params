62ea686e171b199ce2b080586e0b3b10f6ea3102
Nick Kliner <nkliner@chicagobooth.edu>
2017-04-20 16:16:55 -0500

    Square form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,4 +9,18 @@ class CalculationsController < ApplicationController
 
   end
 
+  def square_form
+
+    render("calculations/square_form.html.erb")
+
+  end
+
+  def square
+    # Parameters: {"user_number"=>"66"}
+    @user_number = params[:user_number].to_f
+    @square = @user_number ** 2
+    render("calculations/square.html.erb")
+
+  end
+
 end
