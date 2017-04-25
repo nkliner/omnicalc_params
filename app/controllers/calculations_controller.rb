381fb05528c72c48ea79a09ea4693d9ce22c3c64
Nick Kliner <nkliner@chicagobooth.edu>
2017-04-25 17:14:47 -0500

    payment with form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -21,8 +21,13 @@ class CalculationsController < ApplicationController
   def flex_payment
 
     # Parameters: {"num"=>"5"}
-    @user_number = params["num"].to_f
-    @payment = @user_number
+    @user_term = params["number_of_years"].to_f
+    @user_rate = params["basis_points"].to_f
+    @user_loan = params["present_value"].to_f
+    @numerator = ((@user_rate/120000)*@user_loan)
+    @period = (-@user_term*12)
+    @monthly_rate = (@user_rate/120000)
+    @payment = @numerator/(1-((1+@monthly_rate)**@period))
     render("calculations/flex_payment.html.erb")
 
   end
@@ -36,6 +41,12 @@ class CalculationsController < ApplicationController
   def square_root_form
 
     render("calculations/square_root_form.html.erb")
+
+  end
+
+  def payment_form
+
+    render("calculations/payment_form.html.erb")
 
   end
 
@@ -57,8 +68,13 @@ class CalculationsController < ApplicationController
 
   def payment
     # Parameters: {"user_number"=>"66"}
-    @user_number = params[:user_number].to_f
-    @payment = @user_number
+    @user_term = params["number_of_years"].to_f
+    @user_rate = params["basis_points"].to_f
+    @user_loan = params["present_value"].to_f
+    @numerator = ((@user_rate/120000)*@user_loan)
+    @period = (-@user_term*12)
+    @monthly_rate = (@user_rate/120000)
+    @payment = @numerator/(1-((1+@monthly_rate)**@period))
     render("calculations/payment.html.erb")
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
   get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root"})
-  get("/flexible/payment/:num", {:controller => "calculations", :action => "flex_payment"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", {:controller => "calculations", :action => "flex_payment"})
   get("/square/new", {:controller => "calculations", :action => "square_form"})
   get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+  get("/payment/new", {:controller => "calculations", :action => "payment_form"})
   get("/square/results", {:controller => "calculations", :action => "square"})
   get("/square_root/results", {:controller => "calculations", :action => "square_root"})
   get("/payment/results", {:controller => "calculations", :action => "payment"})
