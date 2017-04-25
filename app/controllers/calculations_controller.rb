feae04807a1f2f4686d517b94e4f86b012be84bd
Nick Kliner <nkliner@chicagobooth.edu>
2017-04-25 15:39:44 -0500

    Square root

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,6 +9,15 @@ class CalculationsController < ApplicationController
 
   end
 
+  def flex_square_root
+
+    # Parameters: {"num"=>"5"}
+    @user_number = params["num"].to_f
+    @square_root = @user_number ** 0.5
+    render("calculations/flex_square_root.html.erb")
+
+  end
+
   def square_form
 
     render("calculations/square_form.html.erb")
@@ -20,6 +29,14 @@ class CalculationsController < ApplicationController
     @user_number = params[:user_number].to_f
     @square = @user_number ** 2
     render("calculations/square.html.erb")
+
+  end
+
+  def square_root
+    # Parameters: {"user_number"=>"66"}
+    @user_number = params[:user_number].to_f
+    @square_root = @user_number ** 0.5
+    render("calculations/square_root.html.erb")
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
+  get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root"})
   get("/square/new", {:controller => "calculations", :action => "square_form"})
   get("/square/results", {:controller => "calculations", :action => "square"})
+  get("/square_root/results", {:controller => "calculations", :action => "square_root"})
 
 end
